919bb382694cc0ac66f2cc69d39fe1f9f822e89e
Jichao Ouyang <oyanglulu@gmail.com>
2015-06-06 00:20:36 +0800

    using form instead

--- src/components/interviewer.jsx
+++ src/components/interviewer.jsx
@@ -7,10 +7,10 @@ var InterviewerForm = React.createClass({
       status: -1
     }
   },
-  handle_submit: async function(){
+  _handleSubmit: async function(e){
+    e.preventDefault();
     var resp = await store.newInterviewer(
       {'name':this.refs.name.value,
-       'tel':this.refs.tel.value,
        'email':this.refs.email.value});
     this.setState({
       status: resp.status.code
@@ -20,7 +20,7 @@ var InterviewerForm = React.createClass({
   render: function(){
     var message;
     if (this.state.status===201){
-       message = (
+      message = (
         <div data-alert className="alert-box status radius">
           interviewer created!
         </div>
@@ -36,11 +36,13 @@ var InterviewerForm = React.createClass({
       <div className='row'>
         <div className="small-11 small-centered large-6 large-centered columns">
           <h2>Create New Interviewer</h2>
-             {message}
-          <Field lname='Name' id='name' ref='name'></Field>
-          <Field lname='Email' id='email' ref='email'></Field>
-          <Field lname='Tel.' id='tel' ref='tel'></Field>
-          <input type='submit' id='submit' className="button" value="Create" onClick={this.handle_submit}/>
+          {message}
+          <form onSubmit={this._handleSubmit} method="post">
+            <Field lname='Name' id='name' ref='name'></Field>
+            <Field lname='Email' id='email' ref='email'></Field>
+            <Field lname='Tel.' id='tel' ref='tel'></Field>
+            <input type='submit' id='submit' className="button" value="Create"/>
+          </form>
         </div>
       </div>
     );
