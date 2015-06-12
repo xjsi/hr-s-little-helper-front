daa5630bd5f41a26dc7cea0e3f92d5d66cf78aee
sunchen115 <sunchen115work@gmail.com>
2015-06-12 15:33:24 +0800

    delete phone, add language experience

--- src/components/interviewers.jsx
+++ src/components/interviewers.jsx
@@ -9,6 +9,8 @@ var InterviewersList = React.createClass({
 	 		<thead>
 			    <td width="200">Name</td>
 			    <td width="500">Email</td>
+			    <td width="500">Language</td>
+			    <td width="200">Experience</td>
 			    <td width="300">Opertions</td>
 		    </thead>
 	  	    <InterviewersListBody/>
