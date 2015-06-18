b3708aaa834cdc09c2d7132e04975f59a49b14e7
Jichao Ouyang <oyanglulu@gmail.com>
2015-06-18 16:20:29 +0800

    auth wrapper for client

--- src/store.js
+++ src/store.js
@@ -2,40 +2,48 @@ var client = require('./client'),
     when = require('when');
 
 var store={};
+
+var authclient = function(opt) {
+  client(opt).then(function(resp) {
+    debugger
+    if(resp.status.code===401) location.href='/login/'
+    return resp
+  })
+}
 store.newInterviewer = function(entity) {
-  return client({method:'POST', path: 'interviewer', entity: entity});
+  return authclient({method:'POST', path: 'interviewer', entity: entity});
 };
 
 store.updateInterviewer = function(entity) {
-  return client({method:'PUT', path: 'interviewer/'+entity.key, entity: entity});
+  return authclient({method:'PUT', path: 'interviewer/'+entity.key, entity: entity});
 };
 
 store.allInterviewers = function(){
-  return client({method:'GET', path: 'interviewers'});
+  return authclient({method:'GET', path: 'interviewers'});
 };
 
 store.interviews = function(){
-  return client({method:'GET', path: 'interviews'});
+  return authclient({method:'GET', path: 'interviews'});
 };
 
 store.getInterview = function(id){
-  return client({method:'GET', path: 'interview/'+id});
+  return authclient({method:'GET', path: 'interview/'+id});
 };
 
 store.viewInterviewer = function(id){
-  return client({method:'GET', path:'interviewer/'+id});
+  return authclient({method:'GET', path:'interviewer/'+id});
 };
 
 store.deleteInterviewer = function(id){
-  return client({method:'DELETE',path:'interviewer/'+id});
+  return authclient({method:'DELETE',path:'interviewer/'+id});
 };
 
 store.newInterview = function(entity) {
-  return client({method:'POST', path: 'interview', entity: entity});
+  return authclient({method:'POST', path: 'interview', entity: entity});
 }
 
 store.editInterview = function(id, entity) {
-  return client({method:'PUT', path: 'interview/'+id, entity: entity});
+  return authclient({method:'PUT', path: 'interview/'+id, entity: entity});
 }
 
 module.exports = store;
