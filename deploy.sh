d47f7cc55e445080a991c62f96ce328e025ee034
Jichao Ouyang <oyanglulu@gmail.com>
2015-06-18 11:28:38 +0800

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
-git fetch origin
-git checkout -b gh-pages origin/gh-pages
+./node_modules/gulp/bin/gulp.js build sass
+git checkout -b gh-pages
 git status
+git rm -r . --cache
 git add javascripts stylesheets index.html login
 git commit -m "publi:ship: $CIRCLE_BUILD_NUM"
-git push origin gh-pages
+git push origin gh-pages -f
